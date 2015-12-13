72de3f8647867bd43a55b53067407fa8368c5de6
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2015-12-13 01:52:01 +0000

    Particle code

---
+++ particle/irremote.ino
@@ -0,0 +1,54 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "IRremote.h"
+
+IRsend irsend(D2);
+
+//int rawCode = 0xFFA857;
+
+String result;
+
+void setup() {
+
+    Serial.begin(9600);
+    Serial1.begin(9600);
+    
+    // Able request to record IR-signals
+    Particle.function("emitIRSignals", emitIRSignals);
+    
+    // Able request to send IR-signals
+    Particle.function("sendIRSignal", sendIRSignal);
+    
+}
+
+void loop() {
+
+
+}
+
+int emitIRSignals(String command) {
+    if(command == "start" && Serial1.available()) {
+        // Read serial data from arduino
+        result = Serial1.readStringUntil('\n');
+        
+        Particle.publish(result);
+        
+        return 0;
+    }
+    else if(command == "stop") {
+        return 0;
+    }
+    else {
+        return -1;
+    }
+}
+
+int sendIRSignal(String irSignal) {
+
+    const char *hexstring = "0x" + irSignal;
+    int number = (int)strtol(hexstring, NULL, 0);
+    Serial.println(number);
+    
+    irsend.sendNEC(number, 32);
+    
+    return 0;
+}
